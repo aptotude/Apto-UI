0a5475475d32e6ccd9d31b8063a5be42b94007b2
James Olson <jolson@apto.com>
2018-07-03 16:02:17 -0600

    configure storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,17 @@
 import { configure } from '@storybook/angular';
+import { setOptions } from '@storybook/addon-options';
 
 const req = require.context('../src/components', true, /\.stories\.ts$/)
 
 function loadStories() {
-  req.keys().forEach((filename) => req(filename));
+    req.keys().forEach((filename) => req(filename));
 }
+
+setOptions({
+    name: 'Apto UI',
+    url: '/#',
+    showAddonPanel: false,
+    showStoriesPanel: true
+});
 
 configure(loadStories, module);
