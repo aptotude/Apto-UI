2c1c4e95997f9e539480f16410d0ef87e722bd65
James Olson <jolson@apto.com>
2018-08-02 18:19:30 -0600

    Fix packaging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AptoButtonComponentModule } from './lib/button/button.module';
-import { AptoTooltipComponentModule } from './lib/tooltip/tooltip.module';
-import { AptoGridComponentModule } from './lib/grid/grid.module';
 
 @NgModule({
     imports: [
-        BrowserModule,
-        AptoButtonComponentModule,
-        AptoTooltipComponentModule,
-        AptoGridComponentModule
+        BrowserModule
     ],
     declarations: [],
     providers: [],
